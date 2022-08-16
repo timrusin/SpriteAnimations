626369a633145d1043d2a9a57b002ff053b7c5e4
timrusin <timrusin@gmail.com>
2022-08-16 16:18:27 -0500

    setup project and am up to 15 min in project 1 of sprite animations

---
+++ script.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'images/shadow_dog.png'
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+
+function animate(){
+    ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
+    // ctx.fillRect(50,50,100,100);
+    // ctx.drawImage(playerImage, sx, sy, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    requestAnimationFrame(animate);
+};
+animate();
+
