132dadff85be7a82454ea2c983262522e3f0b4fc
timrusin <timrusin@gmail.com>
2022-08-16 22:30:37 -0500

    refactored with cleaner code

--- script.js
+++ script.js
@@ -14,13 +14,10 @@ const staggerFrames = 10;
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
-    // ctx.fillRect(50,50,100,100);
-    // ctx.drawImage(playerImage, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
-    if (gameFrame %  staggerFrames == 0){
-        if (frameX < 6) frameX++;
-        else frameX = 0 
-    }
+    let position = Math.floor(gameFrame/staggerFrames) % 6; 
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  
 
     gameFrame++;
     requestAnimationFrame(animate);
