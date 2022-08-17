3255c884f65b84477004f6bb36aef865c4bd7201
timrusin <timrusin@gmail.com>
2022-08-16 22:19:31 -0500

    animation happening staggerFrame allows for speed control

--- script.js
+++ script.js
@@ -9,13 +9,22 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 10;
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
     // ctx.fillRect(50,50,100,100);
     // ctx.drawImage(playerImage, sx, sy, sw, sh, dx, dy, dw, dh);
     ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    if (gameFrame %  staggerFrames == 0){
+        if (frameX < 6) frameX++;
+        else frameX = 0 
+    }
+
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 animate();
 
+// 21 min
